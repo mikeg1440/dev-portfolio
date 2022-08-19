21f4305d739d88e30d7d740a92209940835b4f5a
mikeg1440 <gaudreaum01@gmail.com>
2022-08-19 15:59:50 -0400

    Fix issue causing error in console

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ class App extends Component {
       cache: false,
       success: function (data) {
         this.setState({ sharedData: data });
-        document.title = `${this.state.sharedData.basic_info.name}`;
+        document.title = `${this.state.sharedData.basic_info && this.state.sharedData.basic_info.name}`;
       }.bind(this),
       error: function (xhr, status, err) {
         alert(err);
