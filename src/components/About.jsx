2bcf0c816d7e05987833fe4b6000968c99a08e17
mikeg1440 <gaudreaum01@gmail.com>
2022-08-19 15:59:17 -0400

    Rename js components to jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+// import angularIcon from "@iconify/icons-logos/angular-icon";
+// import reactIcon from "@iconify/icons-logos/react";
+// import vueIcon from "@iconify/icons-logos/vue";
 
 class About extends Component {
   render() {
@@ -31,15 +31,12 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
-                    icon={reactIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
-                    icon={vueIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
