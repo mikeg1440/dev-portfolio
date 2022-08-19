4f29ad1898a2eede0a0bc2386a4688594326dd8c
mikeg1440 <gaudreaum01@gmail.com>
2022-08-19 16:00:12 -0400

    Add missing env variables that we check to see what version of the site we want to load

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from './App'
 import './index.scss';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Need these env variables to be set for this to work
+// gives us the ability to use a alt language 
+window.$primaryLanguage = 'en';
+window.$secondaryLanguage = 'pl';
+window.$primaryLanguageIconId = 'primary-lang-icon';
+window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
