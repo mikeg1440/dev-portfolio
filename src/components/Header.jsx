1537bd75cbc0ffe83582775efdff407362bdeaaa
mikeg1440 <gaudreaum01@gmail.com>
2022-08-30 17:33:32 -0400

    Add glox effect to name in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ class Header extends Component {
             <div>
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
               <br/>
-              <h1 className="mb-0">
+              <h1 className="mb-0 flux-glow">
                 <Typical steps={[name]} wrapper="p" />
               </h1>
               <div className="title-container">
