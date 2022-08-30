455738f97862f76dabd22d32aa3401ebab4e42f1
mikeg1440 <gaudreaum01@gmail.com>
2022-08-29 21:32:21 -0400

    Fix experience badges so they are more readable now

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,21 +16,21 @@ class Experience extends Component {
 
         var mainTech = mainTechnologies.map((technology, i) => {
           return (
-            <Badge pill className="main-badge mr-2 mb-2" key={i}>
+            <div className="main-badge badge rounded-pill mr-2 mb-2" key={i}>
               {technology}
-            </Badge>
+            </div>
           );
         });
         var tech = technologies.map((technology, i) => {
           return (
-            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+            <div className="experience-badge badge rounded-pill mr-2 mb-2" key={i}>
               {technology}
-            </Badge>
+            </div>
           );
         });
         return (
           <VerticalTimelineElement
-            className="vertical-timeline-element--work"
+            className="vertical-timeline-element--work custom-card"
             date={work.years}
             iconStyle={{
               background: "#AE944F",
