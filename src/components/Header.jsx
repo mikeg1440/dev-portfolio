e61289772984b809039c879078ad3ff730200a37
Mike Gaudreau <gaudreaum01@gmail.com>
2024-02-01 17:18:29 -0500

    Add CSS classes to fix text sizing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ class Header extends Component {
             <div>
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
               <br/>
-              <h1 className="mb-0 flux-glow">
+              <h1 className="mb-0 flux-glow name-wrapper">
                 <Typical steps={[name]} wrapper="p" />
               </h1>
               <div className="title-container">
